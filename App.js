a5bb3596e64e1fa00b4e74ddea922703d982837c
londtreeforce <londtreeforce@gmail.com>
2023-03-05 20:44:21 +0300

    Worked Loading Screen and accept geoLocation for API v2

--- App.js
+++ App.js
@@ -4,30 +4,45 @@ import React from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
+import axios from 'axios';
 
-const API_KEY = '968084f7f15501325343a32afa1a645a';
+
+const API_KEY = '46045c82453014d02ee6311a65fc4b17';
 
 
 export default class extends React.Component {
   state = {
-    isLoading: true
+    isLoading: false
   }
 
 
 
+
+  getWeather = async (latitude, longitude) => {
+
+    const {data} = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+      console.log(data);
+  }
+
+
+  coordinatToServer = async (latitude, longitude) => {
+    //Тут axios'ом послать запрос на свой сервер
+  }
+
   getLocation = async () => {
     try {
       
-      
-      
+      //https://api.openweathermap.org/data/2.5/weather?lat=59.7957572&lon=30.6353518&appid={46045c82453014d02ee6311a65fc4b17}
+      //http://api.openweathermap.org/geo/1.0/reverse?lat=59.7957572&lon=30.6353518&limit={limit}&appid=46045c82453014d02ee6311a65fc4b17
       //POST GET PUT And Other, it's accept geoPosition
       //Здесь нужен какой-то скорее трекинг местоположения сделать
       //Сейчас координаты будут отправляться на api , можно так же сделать функцию с отправкой на сервер
       await Location.requestBackgroundPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      this.coordinatToServe(latitude, longitude); 
       this.setState({isLoading: false});      
-      console.log(coords.latitude, coords.longitude);
-      
     } catch (error) {
       Alert.alert('Жду принятия  ' ,'Примите соглашение на отпраку геоданных для подбора погоды...')
     }
